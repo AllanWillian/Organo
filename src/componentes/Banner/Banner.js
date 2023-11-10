96d31586c7387a5edcebea9721ed88ab511a9b57
Allan Willian <80597337+AllanWillian@users.noreply.github.com>
2023-11-10 00:01:24 -0300

    Add files via upload

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+const Banner= () => {
+    return (
+        <header className='banner'>
+            <img src="/img/banner.png" alt="O banner principal da página"></img>
+        </header>
+    )
+}
+
+export default Banner;
